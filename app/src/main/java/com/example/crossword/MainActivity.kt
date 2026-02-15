3737a4f2aafb7d3d6da46ebac603d50f1b4ed739
nikov3 <nikovn557@gmail.com>
2026-02-15 21:11:32 +0200

    Updated Hello message

--- app/src/main/java/com/example/crossword/MainActivity.kt
+++ app/src/main/java/com/example/crossword/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             CrosswordTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Niki",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     CrosswordTheme {
-        Greeting("Android")
+        Greeting("Niki")
     }
 }
